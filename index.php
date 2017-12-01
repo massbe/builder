9370bc2cb424d2e6be6bafca9eb6cdebe77d7918
Luther King <massbevbp@gmail.com>
2017-12-01 16:34:58 +0200

    fine

--- index.php
+++ index.php
@@ -4,12 +4,11 @@ include_once ('vendor/autoload.php');
 use Components\SelectQueryBuilder;
 
 $table = 'newTable';
-$arrayColums = array('id', 25, 2543, 'assad');
+$arrayColums = ['id', 25, 2543, 'assad'];
 
 try {
     $obj = new SelectQueryBuilder($table);
-    $obj->colums($arrayColums)->order('id')->limit(10);
-    $obj->get();
+    $obj->colums($arrayColums)->order('id')->limit(10)->get();
 } catch (Exception $e) {
     echo "Error: ".$e->getMessage();
 }
